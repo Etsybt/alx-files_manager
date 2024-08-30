76dbafc8c91a1bc3fb6b981de46185b62de3cb0b
Etsybt <nouhailahmidouch2@gmail.com>
2024-08-30 16:37:50 +0000

    update

---
+++ controllers/UsersController.js
@@ -0,0 +1,47 @@
+import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
+
+class UsersController {
+  static async postNew(req, res) {
+    const { email, password } = req.body;
+    if (!email) {
+      return res.status(400).json({ error: 'Missing email' });
+    }
+    if (!password) {
+      return res.status(400).json({ error: 'Missing password' });
+    }
+
+    const userExists = await dbClient.getUser({ email });
+    if (userExists) {
+      return res.status(400).json({ error: 'Already exist' });
+    }
+
+    const hashedPassword = crypto.createHash('sha1').update(password).digest('hex');
+    const newUser = { email, password: hashedPassword };
+    const userId = await dbClient.createUser(newUser);
+
+    return res.status(201).json({ id: userId, email });
+  }
+
+  static async getMe(req, res) {
+    const token = req.headers['x-token'];
+    if (!token) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const key = `auth_${token}`;
+    const userId = await redisClient.get(key);
+    if (!userId) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const user = await dbClient.getUser({ _id: userId });
+    if (!user) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    return res.status(200).json({ id: user._id, email: user.email });
+  }
+}
+
+export default UsersController;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 import express from 'express';
-import AppController from '../controllers/AppController';
+import FilesController from '../controllers/FilesController';
 
 const router = express.Router();
 
-router.get('/status', AppController.getStatus);
-router.get('/stats', AppController.getStats);
+router.get('/status', FilesController.getStatus);
+router.get('/stats', FilesController.getStats);
+router.post('/users', FilesController.postUser);
+router.get('/connect', FilesController.getConnect);
+router.get('/disconnect', FilesController.getDisconnect);
+router.get('/users/me', FilesController.getMe);
+router.post('/files', FilesController.postUpload);
+router.get('/files/:id', FilesController.getShow); // New endpoint
+router.get('/files', FilesController.getIndex); // New endpoint
 
 export default router;
