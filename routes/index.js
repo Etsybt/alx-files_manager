505bde6723874be1f3d1c4ed6e1dcfe6ebe326be
Etsybt <nouhailahmidouch2@gmail.com>
2024-08-31 15:08:23 +0000

    update

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
+import AppController from '../controllers/AppController';
+
 const express = require('express');
+
 const router = express.Router();
 const UsersController = require('../controllers/UsersController');
-import AppController from '../controllers/AppController.js';
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
 router.post('/users', UsersController.postNew);
-
 
 module.exports = router;
