e60c693b32ad0b0e748094ea89c5c6ad56e35c79
Etsybt <nouhailahmidouch2@gmail.com>
2024-08-31 12:43:18 +0000

    update

--- utils/db.js
+++ utils/db.js
@@ -39,9 +39,7 @@ class DBClient {
   }
 
   async getUser(query) {
-    console.log('QUERY IN DB.JS', query);
     const user = await this.db.collection('users').findOne(query);
-    console.log('GET USER IN DB.JS', user);
     return user;
   }
 
