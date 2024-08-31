891069ecc0e31c881d2dac4bb002b4272fcaab8a
Etsybt <nouhailahmidouch2@gmail.com>
2024-08-31 12:30:26 +0000

    update

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,9 @@
 import express from 'express';
-import FilesController from '../controllers/FilesController';
+import AppController from '../controllers/AppController';
 
 const router = express.Router();
 
-router.get('/status', FilesController.getStatus);
-router.get('/stats', FilesController.getStats);
-router.post('/users', FilesController.postUser);
-router.get('/connect', FilesController.getConnect);
-router.get('/disconnect', FilesController.getDisconnect);
-router.get('/users/me', FilesController.getMe);
-router.post('/files', FilesController.postUpload);
-router.get('/files/:id', FilesController.getShow); // New endpoint
-router.get('/files', FilesController.getIndex); // New endpoint
+router.get('/status', AppController.getStatus);
+router.get('/stats', AppController.getStats);
 
 export default router;
